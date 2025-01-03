51f005a7315cc58306d79228368e37b7c448c580
CARDONA LUIS <kikecar97@gmail.com>
2025-01-03 08:05:26 -0600

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI, Depends
+from fastapi.middleware.cors import CORSMiddleware
 from sqlalchemy.orm import Session
 from app.database.connection import get_db
 from app.routers import empleadoRoute, permisoPersonalRoute, authRoute
@@ -8,6 +9,20 @@ from dotenv import load_dotenv
 
 app = FastAPI()
 load_dotenv()
+
+# Configuración de CORS
+origins = [
+    "http://localhost:4200",
+    # Agrega aquí otros orígenes permitidos si es necesario
+]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/")
 def read_root(db: Session = Depends(get_db)):
