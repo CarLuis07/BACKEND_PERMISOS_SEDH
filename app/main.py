0483837728072874e231bcdeb194e6410a05a9ba
CARDONA LUIS <kikecar97@gmail.com>
2025-02-05 14:30:44 -0600

    update

--- app/main.py
+++ app/main.py
@@ -15,7 +15,7 @@ load_dotenv()
 # Configuración de CORS
 origins = [
     "http://localhost:4200",
-    "http://192.168.180.26",
+    "http://192.168.181.113",
     "http://192.168.180.26:8000",
     # Agrega aquí otros orígenes permitidos si es necesario
 ]
